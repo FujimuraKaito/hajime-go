a2dfa8d93a2a30cfd16896a20cc5782e8b8b1f8c
FujimuraKaito <kaito.bakuman@gmail.com>
2021-09-14 00:02:50 +0900

    🎨 add grammar

--- first/myproject/src/gosample/gosample.go
+++ first/myproject/src/gosample/gosample.go
@@ -1,3 +1,10 @@
 package gosample
 
 var Message string = "hello world"
+
+var (
+	a string = "aaa"
+	b        = "bbb"
+	c        = "ccc"
+	d        = "ddd"
+)
--- first/myproject/src/main/main.go
+++ first/myproject/src/main/main.go
@@ -1,10 +1,55 @@
 package main
 
 import (
-	"fmt"
+	f "fmt"
 	"gosample"
 )
 
 func main() {
-	fmt.Println(gosample.Message)
+	f.Println(gosample.Message)
+
+	message := "temp message"
+	f.Println(message)
+
+	// constant
+	const Hello string = "hello world"
+	f.Println(Hello)
+
+	// ゼロ値（型によって初期化される値）
+	var i int
+	f.Println(i) // 0
+
+	a, b := 10, 100
+	if a > b {
+		f.Println("a is larger than b")
+	} else if a < b {
+		f.Println("a is smaller than b")
+	} else {
+		f.Println("a equals b")
+	}
+
+	for i := 0; i < 3; i++ {
+		f.Println(i)
+	}
+
+	// how to use while
+	n := 0
+	for n < 5 {
+		f.Printf("n = %d\n", n)
+		n++
+	}
+
+	n = 0
+	for {
+		n++
+		if n > 10 {
+			break
+		}
+		if n%2 == 0 {
+			// 偶数なら処理を飛ばす
+			continue
+		}
+		f.Println(n)
+	}
+
 }
